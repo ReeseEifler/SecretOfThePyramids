3dcc0866b9afab61e0eb0aa127fb0ed985e5543a
unknown <reeseeifler@gmail.com>
2014-09-09 12:07:35 -0400

    second commit with start of first method

--- SecretOfThePyramids/Program.cs
+++ SecretOfThePyramids/Program.cs
@@ -11,6 +11,7 @@ namespace SecretOfThePyramids
     {
         static void Main(string[] args)
         {
+            /*
             StreamReader myReader = new StreamReader("TextFile1.txt");
             string line = "";
 
@@ -23,6 +24,27 @@ namespace SecretOfThePyramids
 
             myReader.Close();
             Console.ReadLine();
+            */
+            string firstPrompt = textFileReader();
+            Console.WriteLine(firstPrompt)
         }
+       private string textFileReader()
+        {
+            //this is the method that will read each text file
+            string textFile = "TextFile0";
+            StreamReader myReader = new StreamReader(textFile);
+            string line = "";
+
+            while (line != null)
+            {
+                line = myReader.ReadLine();
+                //if (line != null)
+                  //  Console.WriteLine(line);
+            }
+
+            //myReader.Close();
+            //Console.ReadLine();
+            return line;
+        } 
     }
 }
