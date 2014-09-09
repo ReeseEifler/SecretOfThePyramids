2a05c32cf90eb33e242819167d2fb0e02ddff15c
unknown <reeseeifler@gmail.com>
2014-09-09 14:12:54 -0400

    first decision prototype

--- SecretOfThePyramids/Program.cs
+++ SecretOfThePyramids/Program.cs
@@ -11,28 +11,26 @@ namespace SecretOfThePyramids
     {
         static void Main(string[] args)
         {
-            /*
-            StreamReader myReader = new StreamReader("TextFile1.txt");
-            string line = "";
-
-            while (line != null)
-            {
-                line = myReader.ReadLine();
-                if (line != null)
-                    Console.WriteLine(line);
-            }
-
-            myReader.Close();
-            Console.ReadLine();
-            */
             string firstPrompt = textFileReader("TextFile0.txt");
             Console.WriteLine(firstPrompt);
             Console.ReadLine();
+            Console.WriteLine("Do you want 1 or 2?");
+            string userValue = Console.ReadLine();
+            if (userValue == "1")
+            {
+                Console.WriteLine("Hopefully this will go to page 1!");
+                Console.ReadLine();
+            }
+            else if (userValue == "2")
+            {
+                Console.WriteLine("This should go to page 2.");
+                Console.ReadLine();
+            }
         }
        private static string textFileReader(string textFile)
         {
             //this is the method that will read each text file
-            //string textFile = "TextFile0";
+            
             StreamReader myReader = new StreamReader(textFile);
             string line = "";
 
@@ -42,9 +40,7 @@ namespace SecretOfThePyramids
                 if (line != null)
                     Console.WriteLine(line);
             }
-
             myReader.Close();
-            //Console.ReadLine();
             return line;
         } 
     }
